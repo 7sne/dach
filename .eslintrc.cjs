9a90f98413bff266363299e80f02f6f2ef9c7719
7sne <kooblatum@gmail.com>
2023-11-09 00:10:47 +0100

    Update `eslint` config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,15 @@
 // @ts-check
 
 module.exports = {
-    extends: '@antfu',
+    extends: ['@antfu'],
     rules: {
         'arrow-parens': ['error', 'as-needed'],
-        'indent': ['error', 4],
+        'quote-props': ['error', 'as-needed'],
+        indent: ['error', 4],
         '@typescript-eslint/indent': ['error', 4],
         '@typescript-eslint/brace-style': 'off',
         '@typescript-eslint/no-redeclare': 'off',
-        '@typescript-eslint/consistent-type-definitions': [
-            'error',
-            'type',
-        ],
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
         '@typescript-eslint/no-use-before-define': 'off',
         'global-require': 0,
     },
